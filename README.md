ff64b9e7a856a888ab3b845d8a7433f61a23cc4f
Andreas Hocevar <andreas.hocevar@gmail.com>
2016-09-30 11:27:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Mapbox to CSS Font
+
+Utility to convert Mapbox GL Style font names to CSS compatible font definitions.
+
+## API
+
+### parseFont
+
+Utility to convert Mapbox GL Style font names to CSS font definitions.
+
+**Parameters**
+
+-  `font` **([string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** Mapbox GL Style font name, e.g. `'Open Sans Regular'`.
+
+Returns **[Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)** Parts of the CSS font definition, i.e. `[font-style font-weight font-size font-family]`. The size will be `16`. Call
+`asCss` with this array and a size to get a CSS font definition string.
+
+### asCss
+
+Applies a size to an array of CSS font definition parts and returns a CSS font definition string.
+
+**Parameters**
+
+-  `cssData` **[Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)** Parts of the CSS font definition, i.e. `[font-style font-weight font-size font-family]`.
+
+-  `size` **[number](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number)** Font size in pixels.
+
+Returns **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** CSS font definition, e.g. `'normal 400 16px Open Sans'`.
---
+++ index.js
@@ -0,0 +1,63 @@
+var fontWeights = {
+  thin: 100,
+  hairline: 100,
+  'ultra-light': 100,
+  'extra-light': 100,
+  light: 200,
+  book: 300,
+  regular: 400,
+  normal: 400,
+  plain: 400,
+  roman: 400,
+  standard: 400,
+  medium: 500,
+  'semi-bold': 600,
+  'demi-bold': 600,
+  bold: 700,
+  heavy: 800,
+  black: 800,
+  'extra-bold': 800,
+  'ultra-black': 900,
+  'extra-black': 900,
+  'ultra-bold': 900,
+  'heavy-black': 900,
+  fat: 900,
+  poster: 900
+};
+
+var fontMap = {};
+
+function asCss(cssData, size) {
+  css[2] = size + 'px';
+  return fontData.join(' ');
+}
+
+function parseFont(font) {
+  var parts = font.split(' ');
+  var maybeWeight = parts[parts.length - 1].toLowerCase();
+  var weight = 'normal';
+  var style = 'normal';
+  if (maybeWeight == 'normal' || maybeWeight == 'italic' || maybeWeight == 'oblique') {
+    style = maybeWeight;
+    parts.pop();
+    maybeWeight = parts[parts.length - 1].toLowerCase();
+  }
+  for (var w in fontWeights) {
+    if (maybeWeight == w || maybeWeight == w.replace('-', '') || maybeWeight == w.replace('-', ' ')) {
+      weight = fontWeights[w];
+      parts.pop();
+      break;
+    }
+  }
+  if (typeof maybeWeight == 'number') {
+    weight = maybeWeight;
+  }
+  var fontFamily = parts.join(' ');
+  // CSS font property: font-style font-weight font-size font-family
+  return [style, weight, 16, fontFamily];
+}
+
+module.exports = {
+  parseFont: parseFont,
+  asCss: fontAsCss
+}
