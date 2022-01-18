ccfc67cd2d8e422d7a8fd0af58f77fcc68dd7109
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-01-18 21:28:05 +0100

    Recognize two-word weights with space

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
-import should from 'should';
-import parseFont from '../index';
+/* eslint-env mocha */
+const should = require('should');
+const parseFont = require('../index.js');
 
 describe('mapbox-to-css-font', function() {
 
@@ -8,6 +9,7 @@ describe('mapbox-to-css-font', function() {
       should(parseFont('Noto Sans', 16)).eql('normal 400 16px "Noto Sans"');
       should(parseFont('Noto Sans Bold', 16)).eql('normal 700 16px "Noto Sans"');
       should(parseFont('Noto Sans SemiBold Italic', 16)).eql('italic 600 16px "Noto Sans"');
+      should(parseFont('Inter Semi Bold', 16)).eql('normal 600 16px Inter');
     });
 
     it('handle line-height', function() {
